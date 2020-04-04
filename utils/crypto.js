e2e39f58b8318ea770ea10a5a1e11d054d50c277
William Clot <william.clot@eurecom.fr>
2020-04-04 17:09:58 +0100

    crypto_utils

--- utils/crypto.js
+++ utils/crypto.js
@@ -6,3 +6,41 @@
  */
 
 const crypto = require('crypto');
+
+getCipherKey = (password) => {
+    const hash = crypto.createHash('sha256');
+    hash.update(password);
+    return hash.digest();
+}
+
+getInitVect = () => {
+    return crypto.randomBytes(16);
+}
+
+getCipher = (cipherKey, initVect) => {
+    const initVect = initVect;
+    const algo = 'aes256';
+    const key = cipherKey;
+    return crypto.createCipheriv(algo, key, initVect);
+  }
+
+/**
+ * encrypt()
+ * data: buffer object
+ * password: string
+ */
+
+encrypt = (data, password) => {
+    const initVect = getInitVect();
+    const cipherKey = getCipherKey(password);
+    const cipher = getCipher(cipherKey,initVect);
+    const ciphertext = cipher.update(data).digest();
+    return initVect + ciphertext;
+  }
+
+decrypt = (initVect, ciphertext, password) => {
+    const cipherKey = getCipherKey(password);
+    const decipher = crypto.createDecipheriv('aes256', cipherKey, initVect);
+    const plaintext = decipher.update(ciphertext);
+    return plaintext
+}
