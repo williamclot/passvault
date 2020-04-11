b0c561ffdd732b8869cc3cd86ac33c72fb66ad45
William CLOT <William.CLOT@amadeus.com>
2020-04-11 22:46:39 +0100

    Updates on files implementation

--- utils/crypto.js
+++ utils/crypto.js
@@ -1,46 +1,42 @@
 /**
- * crypto.js 
- * Functions to encrypt/decrypt files
- * 3/28/2020
- * William Clot
+ * @name crypto.js
+ * @description Functions to encrypt/decrypt data buffers
+ * @author William Clot | Camille Plays
  */
 
 const crypto = require('crypto');
 
 getCipherKey = (password) => {
-    const hash = crypto.createHash('sha256');
-    hash.update(password);
-    return hash.digest();
+  return crypto.createHash('sha256').update(password).digest();
 }
 
 getInitVect = () => {
-    return crypto.randomBytes(16);
+  return crypto.randomBytes(16);
 }
 
 getCipher = (cipherKey, initVect) => {
-    const initVect = initVect;
-    const algo = 'aes256';
-    const key = cipherKey;
-    return crypto.createCipheriv(algo, key, initVect);
-  }
+  const initVect = initVect;
+  const algo = 'aes256';
+  const key = cipherKey;
+  return crypto.createCipheriv(algo, key, initVect);
+}
 
 /**
- * encrypt()
- * data: buffer object
- * password: string
+ * Encryption function
+ * @param buffer data
+ * @param string password
  */
-
 encrypt = (data, password) => {
-    const initVect = getInitVect();
-    const cipherKey = getCipherKey(password);
-    const cipher = getCipher(cipherKey,initVect);
-    const ciphertext = cipher.update(data).digest();
-    return initVect + ciphertext;
-  }
+  const initVect = getInitVect();
+  const cipherKey = getCipherKey(password);
+  const cipher = getCipher(cipherKey, initVect);
+  const ciphertext = cipher.update(data).digest();
+  return initVect + ciphertext;
+}
 
-decrypt = (initVect, ciphertext, password) => {
-    const cipherKey = getCipherKey(password);
-    const decipher = crypto.createDecipheriv('aes256', cipherKey, initVect);
-    const plaintext = decipher.update(ciphertext);
-    return plaintext
+decrypt = (data, password) => {
+  const cipherKey = getCipherKey(password);
+  const decipher = crypto.createDecipheriv('aes256', cipherKey, initVect);
+  const plaintext = decipher.update(ciphertext);
+  return plaintext
 }
--- utils/files.js
+++ utils/files.js
@@ -1,13 +1,23 @@
 /**
- * crypto.js 
- * Functions to encrypt/decrypt files
- * @date: 3/28/2020
- * @author: William Clot
+ * @file files.js 
+ * @description Functions to read/write to files
+ * @author: William Clot | Camille Plays
  */
 
 const fs = require('fs');
+const zlib = require('zlib');
 const os = require('os');
 
-console.log(os.homedir());
-const readStream = fs.createReadStream(os.homedir + '/file.txt');
-readStream.pipe(process.stdout);
+// console.log(os.homedir());
+// const readStream = fs.createReadStream(os.homedir + '/file.txt');
+// readStream.pipe(process.stdout);
+
+readFile = (path) => {
+  const unzipStream = zlib.createGunzip();
+  const readStream = fs.createReadStream(path);
+
+  const data = readStream.pipe(unzipStream);
+  console.log(data);
+};
+
+readFile('./newfile.txt');
